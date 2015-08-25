af6c7c0e8fb2a01398bb4da5125a1de9a55f0c0d
sgund <sabine.gund@uni-hamburg.de>
2015-08-25 12:57:08 +0200

    Remove merge conflict marks, fixed group Id

--- portlets/migration-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/migration/controller/MigrationController.java
+++ portlets/migration-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/migration/controller/MigrationController.java
@@ -1109,7 +1109,7 @@ public class MigrationController {
 			facilityHosts = LegacyFacilityHostLocalServiceUtil.getLegacyFacilityHosts(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
 			logInfo("FacilityHost Size : " + facilityHosts.size());
 			for (LegacyFacilityHost FacilityHost: facilityHosts) {
-				migrateInstitution_Host(FacilityHost, companyId);
+				migrateInstitution_Host(FacilityHost, siteId);
 			} 			
     	
 		} catch (SystemException e1) {
