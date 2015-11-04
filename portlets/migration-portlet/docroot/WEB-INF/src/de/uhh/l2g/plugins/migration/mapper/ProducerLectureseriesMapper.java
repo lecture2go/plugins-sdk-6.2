09dd245a8d2816b77a8737c64a3125461fdce709
Eike Salow <salow@silpion.de>
2015-11-04 12:55:22 +0100

    #90 commit

--- portlets/migration-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/migration/mapper/ProducerLectureseriesMapper.java
+++ portlets/migration-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/migration/mapper/ProducerLectureseriesMapper.java
@@ -9,13 +9,13 @@ import de.uhh.l2g.plugins.model.Producer_Lectureseries;
 public class ProducerLectureseriesMapper {
 	static final Log log = LogFactoryUtil.getLog(ProducerLectureseriesMapper.class);
 
-	public static Producer_Lectureseries mapProducerLectureseries(LegacyProducerLectureseries legacyProducerLectureseries, Producer_Lectureseries producerLectureseries) {
+	public static Producer_Lectureseries mapProducerLectureseries(LegacyProducerLectureseries legacyProducerLectureseries, Producer_Lectureseries producerLectureseries, long companyId) {
 	if (producerLectureseries == null || legacyProducerLectureseries == null) {
 		log.warn("can't execute method with null values - return producerLectureseries as null value");
 		return null;
 	}
 	producerLectureseries.setLectureseriesId(legacyProducerLectureseries.getLectureseriesId());
-	producerLectureseries.setProducerId(legacyProducerLectureseries.getProducerId());
+	producerLectureseries.setProducerId(UserIDMapper.getNewUserId(legacyProducerLectureseries.getProducerId(), companyId));
 	return producerLectureseries;
 	}
 
