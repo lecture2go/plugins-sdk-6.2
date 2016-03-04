306b34e440a0f04f94d5c6df9268fad0e53ceb00
sgund <sabine.gund@uni-hamburg.de>
2016-03-04 12:00:33 +0100

    Statistics Base DB Fuctionality

--- portlets/migration-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/migration/mapper/StatisticsMapper.java
+++ portlets/migration-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/migration/mapper/StatisticsMapper.java
@@ -9,13 +9,13 @@ import de.uhh.l2g.plugins.model.Upload;
 public class StatisticsMapper {
 	static final Log log = LogFactoryUtil.getLog(StatisticsMapper.class);; 
 	
-	public static Upload mapUpload(LegacyStatistics legacyStatistics, Statistics statistics, long companyId) {
+	public static Statistics mapUpload(LegacyStatistics legacyStatistics, Statistics statistics, long companyId) {
 		if (legacyStatistics == null || statistics == null) {
 			log.warn("can't execute method with null values - return statistics as null value");
 			return null;
 		}
-		statistics.setContentLength(legacyStatistics.getContentLength());
-		statistics.setStatus(legacyStatistics.getStatus());
+		statistics.setPrivateVideo(legacyStatistics.getPrivate());
+		statistics.setPublicVideo(legacyStatistics.getPublic());
 		statistics.setTimestamp(legacyStatistics.getTimestamp());
 		statistics.setUserId(UserIDMapper.getNewUserId(legacyStatistics.getUserId(), companyId));
 		statistics.setVideoId(legacyStatistics.getVideoId());
