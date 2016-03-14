612c4511a1b45c05c32711435be673eec3ded861
sgund <sabine.gund@uni-hamburg.de>
2016-03-14 13:37:27 +0100

    Base Classes for Thread Managemnt

---
+++ portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/admin/ThreadManagement.java
@@ -0,0 +1,57 @@
+package de.uhh.l2g.plugins.admin;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.ResourcePermission;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import de.uhh.l2g.plugins.model.Host;
+import de.uhh.l2g.plugins.model.Institution;
+import de.uhh.l2g.plugins.model.Institution_Host;
+import de.uhh.l2g.plugins.service.HostLocalServiceUtil;
+import de.uhh.l2g.plugins.service.InstitutionLocalServiceUtil;
+import de.uhh.l2g.plugins.service.Institution_HostLocalServiceUtil;
+import de.uhh.l2g.plugins.util.PermissionManager;
+
+public class ThreadManagement extends MVCPortlet {
+
+
+	/**Set default permissions (assumes fixed and unique role names)
+	 * 
+	 * @param pm - PermissionManager 
+	 * @throws PortalException 
+	 * @throws SystemException 
+	 */
+	private void setDefaultPermissions(PermissionManager pm) throws SystemException, PortalException{
+		
+
+	}
+	
+	@Override
+	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException, IOException {
+
+		
+		
+
+	}
+}
+
+	
---
+++ portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/util/StatisticsScheduler.java
@@ -0,0 +1,55 @@
+package de.uhh.l2g.plugins.util;
+
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.messaging.MessageListenerException;
+
+import de.uhh.l2g.plugins.service.VideoLocalServiceUtil;
+
+/***************************************************************************
+ * The Lecture2Go software is based on the liferay portal 6.1.1
+ * <http://www.liferay.com>
+ *
+ * Lecture2Go <http://lecture2go.uni-hamburg.de> is an open source
+ * platform for media management and distribution. Our goal is to
+ * support the free access to knowledge because this is a component
+ * of each democratic society. The open source software is aimed at
+ * academic institutions and has to strengthen the blended learning.
+ *
+ * All Lecture2Go plugins are continuously being developed and improved.
+ * For more details please visit <http://lecture2go-open-source.rrz.uni-hamburg.de>
+ *
+ * @Autor Lecture2Go Team
+ * @Version 1.0
+ * @Contact lecture2go-open-source@uni-hamburg.de
+ *
+ * Copyright (c) 2013 University of Hamburg / Computer and Data Center (RRZ)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
+/**There is no built-in time based sheduler in Java 
+ * Statistics would be more reliable on doing snapshot at concrete time (ideally arround midnight) 
+ * (https://quartz-scheduler.org/)
+ */
+/**
+ * The Class VideoStatisticsThread.
+ */
+public class StatisticsScheduler implements MessageListener {  
+    public void receive(Message arg0) throws MessageListenerException {  
+     // TODO Auto-generated method stub : your job code  
+     //Write code : what you want to execute on timely basis.  
+    	System.out.println("Testing liferay corn every 5 minutes");
+ }  
+ } 
