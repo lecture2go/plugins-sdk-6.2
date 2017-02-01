89d890bb11855ce3b8458130ff6264a0d6ac7b83
MathildaM <adatwo@web.de>
2017-02-01 16:55:35 +0100

    #635 Creator fullname has leading space in database if no jobtitle is given

--- portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/admin/CreatorsManagement.java
+++ portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/admin/CreatorsManagement.java
@@ -48,17 +48,20 @@ public class CreatorsManagement extends MVCPortlet {
 		String mn=request.getParameter("middleName");
 		String ln=request.getParameter("lastName");
 		String t=request.getParameter("jobTitle");
-		Creator creator = CreatorLocalServiceUtil.getCreator(reqCreatorId);
-		creator.setFirstName(fn);
-		creator.setMiddleName(mn);
-		creator.setLastName(ln);
-		creator.setJobTitle(t);
-		CreatorLocalServiceUtil.updateCreator(creator);
-		try {
-			response.sendRedirect(backURL);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}		
+		if (isValid(fn, ln)) {
+			Creator creator = CreatorLocalServiceUtil.getCreator(reqCreatorId);
+			creator.setFirstName(fn);
+			creator.setMiddleName(mn);
+			creator.setLastName(ln);
+			creator.setJobTitle(t);
+			creator.setFullName(fullName(fn, mn, ln, t));
+			CreatorLocalServiceUtil.updateCreator(creator);
+			try {
+				response.sendRedirect(backURL);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public void delete(ActionRequest request, ActionResponse response) throws SystemException, PortalException{
@@ -71,6 +74,18 @@ public class CreatorsManagement extends MVCPortlet {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private boolean isValid(String fn, String ln) {
+		String _fn = fn.trim();
+		String _ln = ln.trim();
+		return !"".equals(_fn) && !"".equals(_ln) && _fn.length() > 1
+				&& _ln.length() > 1;
+	}
+
+	private String fullName(String fn, String mn, String ln, String t) {
+		return (t.trim() + " " + (fn.trim() + " " + mn.trim()).trim() + " " + ln
+				.trim()).trim();
 	}
 
 }
