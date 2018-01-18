6431d802092199b29dfbdb0b9363a23cca39c304
isturm <iavor.sturm@uni-hamburg.de>
2018-01-18 15:10:29 +0100

    unused migration portlet removed.

--- portlets/migration-portlet/docroot/WEB-INF/sql/create/create-firebird.sql
+++
@@ -1,4 +0,0 @@
-create database 'lportal.gdb' page_size 8192 user 'sysdba' password 'masterkey';
-connect 'lportal.gdb' user 'sysdba' password 'masterkey';
-create table LG_Migration_TestEntityTwo (plid int64 not null primary key,groupId int64,companyId int64);
-create table LG_Migration_Demo_ (id int64 not null primary key,groupId int64,companyId int64);
