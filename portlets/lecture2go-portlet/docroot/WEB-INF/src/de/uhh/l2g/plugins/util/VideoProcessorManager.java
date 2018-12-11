f6de83e23dff278c17410310f17360d0a1292f30
mttsh <matthiashitzler@hitzler.rrz.uni-hamburg.de>
2018-12-11 10:53:48 +0100

    Add missing changes

--- portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/util/VideoProcessorManager.java
+++ portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/util/VideoProcessorManager.java
@@ -24,6 +24,7 @@ import de.uhh.l2g.plugins.service.VideoLocalServiceUtil;
 public class VideoProcessorManager {
 	protected static Log LOG = LogFactoryUtil.getLog(VideoProcessorManager.class.getName());
 
+	
 	/**
 	 * Starts a video conversion for the video with the given id
 	 * @param videoId the id of the video which will be converted
@@ -37,6 +38,87 @@ public class VideoProcessorManager {
 				JSONObject jo = JSONFactoryUtil.createJSONObject();
 				// the video id
 				jo.put("sourceId", videoId);
+				
+				// set the complete path to the file
+				Video video = VideoLocalServiceUtil.getVideo(videoId);
+				String folder = PropsUtil.get("lecture2go.media.repository")+"/"+HostLocalServiceUtil.getByHostId(video.getHostId()).getServerRoot()+"/"+ProducerLocalServiceUtil.getProducer(video.getProducerId()).getHomeDir()+"/";
+				String filePath;
+				if(video.getOpenAccess()==1){
+					filePath = folder + video.getFilename();
+				}else{
+					filePath = folder + video.getSecureFilename();
+				}
+				jo.put("sourceFilePath", filePath);
+			
+				// set whether a smil file should be created
+				jo.put("createSmil", true);
+				
+				// send POST request to video processor
+				try {
+					HttpManager httpManager = new HttpManager();
+					httpManager.setUrl(videoConversionUrl);
+					if (PropsUtil.contains("lecture2go.videoprocessing.basicauth.user") && PropsUtil.contains("lecture2go.videoprocessing.basicauth.pass")) {
+						httpManager.setUser(PropsUtil.get("lecture2go.videoprocessing.provider.basicauth.user"));
+						httpManager.setPass(PropsUtil.get("lecture2go.videoprocessing.provider.basicauth.pass"));
+					}
+					HttpURLConnection conn = httpManager.sendPost(jo);
+					httpManager.close();
+					
+					// check response
+					int responseCode = conn.getResponseCode();
+					if (responseCode == 201) {
+						return true;
+					} else {
+						LOG.error("Failed starting a new video conversion of video with id: " + videoId + ". Responsecode: " + responseCode); 
+						return false;
+					}
+				} catch (IOException e) {
+					LOG.error("Failed connecting to videoprocessor to start a new video conversion of video with id: " + videoId); 
+					//e.printStackTrace();
+					return false;
+				}
+			} catch (SystemException e) {
+				//e.printStackTrace();
+				return false;
+			} catch (PortalException e) {
+				//e.printStackTrace();
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	
+	/**
+	 * Starts a video conversion for the video with the given id
+	 * @param videoId the id of the video which will be converted
+	 */
+	//public static boolean startVideoConversion(Long videoId, String workflow, HashMap<String, String> workflowProperties) {
+	public static boolean startVideoConversion(Long videoId, String workflow, String captionUrl, String layout) {
+		if (PropsUtil.contains("lecture2go.videoprocessing.provider")) {
+			String videoConversionUrl = PropsUtil.get("lecture2go.videoprocessing.provider.videoconversion");
+
+			try {
+				// create json object with the necessary informations for the videoprocessor
+				JSONObject jo = JSONFactoryUtil.createJSONObject();
+				// the video id
+				jo.put("sourceId", videoId);
+				
+				// the workflow
+				jo.put("workflow", workflow);
+				
+				jo.put("captionUrl", captionUrl);
+				jo.put("layout", layout);
+
+				
+				// an arbitrary number workflow properties
+				/*if (!workflowProperties.isEmpty()) {
+					JSONObject jsonWorkflowProperties = JSONFactoryUtil.createJSONObject();
+					for (Map.Entry<String, String> workflowProperty : workflowProperties.entrySet()) {
+						jsonWorkflowProperties.put(workflowProperty.getKey(), workflowProperty.getValue());
+					}
+					jo.put("workflowProperties", jsonWorkflowProperties);
+				}*/
 				
 				// set the complete path to the file
 				Video video = VideoLocalServiceUtil.getVideo(videoId);
