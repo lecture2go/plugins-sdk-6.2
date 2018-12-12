fbc34bee83906e2998aae8366fa91e5ba1aa6a4d
mttsh <matthias.hitzler@uni-hamburg.de>
2018-12-12 10:59:28 +0100

    Optimize automatic caption generation
    * Allow for arbitrary workflow variables
    * Fix a bug where the file processing started automatically for admin
    * Remove trailing space in series title

--- portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/admin/AdminVideoManagement.java
+++ portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/admin/AdminVideoManagement.java
@@ -6,8 +6,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.portlet.ActionRequest;
@@ -468,11 +470,17 @@ public class AdminVideoManagement extends MVCPortlet {
 		
 		if(resourceID.equals("convertVideo")){
 			String workflow = ParamUtil.getString(resourceRequest, "workflow");
-	 	    // TODO: get an map with arbitrary number of key-values (how to do with ParamUtil or ResourceRequest??)
-	 	    String layout = ParamUtil.getString(resourceRequest, "layout");
-	 	    String captionUrl = HtmlUtils.htmlEscape(ParamUtil.getString(resourceRequest, "captionurl"));
-		 	    
-	 	   JSONObject json = JSONFactoryUtil.createJSONObject();
+			String additionalPropertiesString = ParamUtil.getString(resourceRequest, "additionalProperties");
+			
+			// the additionalProperties is given as a stringified JSON-Object, create a real JSON object for further processing
+	 	    JSONObject additionalProperties = null;
+	 	    try {
+	 	    	additionalProperties = JSONFactoryUtil.createJSONObject(additionalPropertiesString);
+	 	    } catch (Exception e) {
+	 	    	
+	 	    }
+	 	    			
+			JSONObject json = JSONFactoryUtil.createJSONObject();
 			// if activated, notify the video processor to convert the video
 			if (PropsUtil.contains("lecture2go.videoprocessing.provider") && (video.getContainerFormat().equalsIgnoreCase("mp4"))) {
 				String videoConversionUrl = PropsUtil.get("lecture2go.videoprocessing.provider.videoconversion");
@@ -483,7 +491,7 @@ public class AdminVideoManagement extends MVCPortlet {
 					isVideoConversionStarted = VideoProcessorManager.startVideoConversion(video.getVideoId());
 				} else {
 					// another workflow is specified, use this
-					isVideoConversionStarted = VideoProcessorManager.startVideoConversion(video.getVideoId(), workflow, captionUrl, layout);
+					isVideoConversionStarted = VideoProcessorManager.startVideoConversion(video.getVideoId(), workflow, additionalProperties);
 				}
 				if (isVideoConversionStarted) {
 					json.put("status", Boolean.TRUE);
