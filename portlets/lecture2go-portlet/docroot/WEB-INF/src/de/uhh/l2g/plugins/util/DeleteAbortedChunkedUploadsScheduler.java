a3d98fdd62a2cfce8933ad07ac01492799457f4d
mttsh <matthias.hitzler@uni-hamburg.de>
2020-05-26 09:42:05 +0200

    RM#1856 - Scheduler for deletion of aborted chunked uploads
    * A scheduler which runs every night at 3 o'clock, which deletes all
    failed upload chunks, which are older than 24 hours

---
+++ portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/util/DeleteAbortedChunkedUploadsScheduler.java
@@ -0,0 +1,98 @@
+package de.uhh.l2g.plugins.util;
+
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.messaging.MessageListenerException;
+import com.liferay.portal.kernel.scheduler.SchedulerEngine;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.service.ServiceContext;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.util.Date;
+
+/**
+ * This scheduler deletes partial files from aborted chunked uploads
+ */
+public final class DeleteAbortedChunkedUploadsScheduler extends PortletScheduler implements MessageListener {
+
+	private static final long serialVersionUID = 1L;
+	private static final String chunkedUploadFileExtension = ".chunk";
+	
+    public DeleteAbortedChunkedUploadsScheduler(){
+    	super();
+    	LOG = LogFactoryUtil.getLog(DeleteAbortedChunkedUploadsScheduler.class.getName());
+    }
+    
+	public DeleteAbortedChunkedUploadsScheduler(String schedulerClassName, ServiceContext serviceContext) {
+		super(AutocompleteScheduler.class.getName(), serviceContext);
+	    this.schedulerClassName = DeleteAbortedChunkedUploadsScheduler.class.getName();
+	    LOG = LogFactoryUtil.getLog(this.schedulerClassName);
+	}
+	
+	@Override
+    public void receive(Message message) throws MessageListenerException {
+		//uncomment for further debug messages
+		//super.receive(message);
+		LOG.info("DeleteAbortedChunkedUploadsScheduler Scheduler running "+message.getValues().get(SchedulerEngine.JOB_NAME).toString()+"...");
+		
+		//Do Job
+		try {
+			DeleteAbortedChunkedUploadsScheduler.deleteAbortedChunkedUploads();
+			LOG.info("DeleteAbortedChunkedUploadsScheduler Scheduler finished.");
+		} catch (IOException e) {
+			LOG.info("DeleteAbortedChunkedUploadsScheduler Scheduler failed.");
+		}
+    }	
+	
+	/**
+	 * Deletes chunks from aborted uploads which are older than a specific threshold
+	 * Checks all subfolders of the media repository for files with the ending ".chunk"
+	 */
+	private static void deleteAbortedChunkedUploads() throws IOException {
+		Path path = Paths.get(PropsUtil.get("lecture2go.media.repository"));
+		final long threshold = 24*60*60*1000; // 24 hours, do not delete files newer than that
+		final long nowInMillis = new Date().getTime();
+
+		// delete files recursively in the media repository
+		Files.walkFileTree(path, new SimpleFileVisitor<Path>(){
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				if(!attrs.isDirectory() && file.toString().toLowerCase().endsWith(chunkedUploadFileExtension)){
+					FileTime lastModifiedDate = attrs.lastModifiedTime();
+					boolean isOlderThanThreshold = (nowInMillis - lastModifiedDate.toMillis()) > threshold;
+			 		if (isOlderThanThreshold) {
+			 			try {
+				 			Files.delete(file);
+			 			} catch(IOException e) {
+			 		        LOG.error("Deletion of " + file + " was not possible.");
+			 			}
+			 		}
+				}
+			    return FileVisitResult.CONTINUE;
+			}
+		});
+	}
+
+	public void start() {
+        super.schedule();
+        LOG.info("DeleteAbortedChunkedUploadsScheduler Scheduler start.");
+	}
+	
+	public void stop() {
+		super.unschedule();
+		LOG.info("DeleteAbortedChunkedUploadsScheduler Scheduler stop.");
+	}
+
+	public void init(String schedulerClassName, ServiceContext serviceContext) {
+        super.initScheduler(schedulerClassName,serviceContext);
+	}
+
+}
