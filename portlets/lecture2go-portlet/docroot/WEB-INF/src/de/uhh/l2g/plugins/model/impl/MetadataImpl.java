0393e8b0d7587eac18a3ef5737ca162874684bed
mttsh <matthias.hitzler@uni-hamburg.de>
2020-08-21 14:10:14 +0200

    RM#1987 - Process descriptions texts to be more accessible
    * Producers may copy and paste arbitrary HTML via the WYSIWYG-editor in
    the description field
    * These data may include missing tags, wrongly used tag etc.
    * Previously only styles were removed on output, this is an attempt to
    process the html to automatically remove tags and attributes, fix
    wrongly used or inaccessible tags and add tags where necessary.
    * Overall the returned text should now be more structured (full use of
    paragraph elements), well formed and should not contain unnecessary
    elements 
    * JSOUP is used for processing

--- portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/model/impl/MetadataImpl.java
+++ portlets/lecture2go-portlet/docroot/WEB-INF/src/de/uhh/l2g/plugins/model/impl/MetadataImpl.java
@@ -14,6 +14,21 @@
 
 package de.uhh.l2g.plugins.model.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Whitelist;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Tag;
+
+
 /**
  * The extended model implementation for the Metadata service. Represents a row in the &quot;LG_Metadata&quot; database table, with each column mapped to a property of this class.
  *
@@ -36,6 +51,109 @@ public class MetadataImpl extends MetadataBaseImpl {
 	
 	@Override
 	public String getDescription() {
-		return super.getDescription().replaceAll("(style|class)=\"[^\"]*\"", "");
+		return makeDescriptionMoreAccessible(super.getDescription());
+	}
+	
+	/**
+	 * Makes the description more accessible by replacing, adding and removing certain html tags
+	 * @param description the unfiltered description from the database
+	 * @return the cleaned description with better html syntax to be more accessible
+	 */
+	private String makeDescriptionMoreAccessible(String description) {
+		Document doc = Jsoup.parseBodyFragment(description);
+		
+		// replace italic (<i>) with <em> tag
+		Elements italicElements = doc.select("i");
+		italicElements.tagName("em");
+		
+		// replace all header elements with <p> in conjunction with <strong> tags, as headers are not allowed in the editor but may be pasted
+		Elements headerElements = doc.select("h1, h2, h3, h4, h5, h6");
+		headerElements.tagName("strong");
+		headerElements.wrap("<p></p>");
+		
+		// replace all <div> elements with <p> to not remove line breaks
+		Elements divElements = doc.select("div");
+		divElements.tagName("p");
+		
+		// Clean the html - the whitelist restricts the html to the same elements as the WYSIWYG-editor
+		Whitelist whitelist = Whitelist.none();
+		whitelist.addTags("a", "p", "strong", "em", "strike", "ul", "ol", "li", "br", "img");
+		whitelist.addAttributes("a", "href", "target", "title");
+		whitelist.addProtocols("a", "href", "http", "https", "mailto");
+		whitelist.addAttributes("img", "align", "alt", "height", "src", "title", "width");
+		whitelist.addProtocols("img", "src", "http", "https", "/");
+		whitelist.preserveRelativeLinks(true);
+		// the placeholder url is necessary to allow for relative linking
+		String accessibleDescription = Jsoup.clean(doc.body().html(),  "http://example.com/", whitelist);
+
+		/* Now it gets a bit complicated. Descriptions with nodes which are not part of an element (text only nodes) will be conveyed into a paragraph tag
+		 * until the next grouping element is reached, which is <p>, <ul>, <ol>, whereas phrasing content like <a>, <strong> and others should be included 
+		 * in the paragraph
+		 */
+		List<TextNode> textNodes = doc.body().textNodes();
+		// empty text nodes may be returned, so to a manual check if "real" text nodes exist
+		boolean hasTextNodes = false;
+		for (TextNode textNode: textNodes) {
+			if (!textNode.text().trim().isEmpty()) {
+				hasTextNodes = true;
+			}
+		}
+		
+		if (hasTextNodes) {
+			// use the already cleaned html code as starting point
+			doc = Jsoup.parseBodyFragment(accessibleDescription);
+
+			// the grouping elements
+			String[] groupingElements = new String[] {"p","ul","ol"};
+			
+			List<Node> nodes = doc.body().childNodesCopy();
+
+			// we rebuild the entire html structure from scratch, so remove old nodes
+	        doc.body().empty();
+			
+			boolean isParagraphCurrentlyComposing = false;
+			Element paragraphElement = null;
+	        for ( Node node : nodes ) {
+	    		if ((node instanceof Element) && (!isParagraphCurrentlyComposing)) {
+	    			// node is element and no paragraph is currently composing -> add it as a child to the body
+        			doc.body().appendChild(node);
+	        	}
+	    		if ((node instanceof TextNode) && (!isParagraphCurrentlyComposing)) {
+	    			if (!((TextNode) node).text().trim().isEmpty()) {
+	    				// is text node and no paragraph is currently composing -> create new paragraph-element
+	        			paragraphElement = new Element(Tag.valueOf("p"), "");
+	            		isParagraphCurrentlyComposing = true;
+	    			}
+	        	}
+	    		if (isParagraphCurrentlyComposing) {
+	            	if (node instanceof Element) {
+	                	// is Element and paragraph is currently composing
+	            		Element element = (Element) node;
+	            		// check if element is a grouping element
+	            		if (!Arrays.asList(groupingElements).contains(element.tagName())) {
+	                    	// Element is not a grouping element -> add as child to the paragraph which is currently composed
+	            			paragraphElement.appendChild(element);
+	            		} else {
+	            			// Element is a grouping element, so we end the composing of the current paragraph 
+	            			// and add the composed paragraph to the body as well as the current paragraph
+	            			doc.body().appendChild(paragraphElement);
+	            			doc.body().appendChild(node);
+	            			isParagraphCurrentlyComposing = false;
+	            		}
+	            	} else if (node instanceof TextNode) {
+	            		// is text node and a paragraph is currently composing  -> add to paragraph-element
+	            		paragraphElement.append(node.toString());
+	            	}
+	    		}
+	        }
+	        // close paragraph is there is still one composing after all nodes are traversed
+	        if (isParagraphCurrentlyComposing) {
+				doc.body().appendChild(paragraphElement);
+	        }
+
+	        accessibleDescription = doc.body().html();
+		}
+		
+		return accessibleDescription;
 	}
 }
